662205db41d9ea6366d61dbf3cb614c3fa54cb47
Pranab Kalita <pavankalita@gmail.com>
2021-07-17 22:41:48 +0530

    Designed the Post timeline

---
+++ client/components/AppPost.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex items-center justify-between border-b border-gray-300 p-3">
+    <div class="flex">
+      <img src="abc" alt="" class="w-12 h-12 rounded-full mr-4">
+
+      <div class="">
+        <span class="font-semibold">Pranab</span> <span class="text-sm text-gray-500">time posted</span>
+
+        <p>Post Body</p>
+      </div>
+    </div>
+
+    <div>
+      <AppPostLike />
+    </div>
+  </div>
+</template>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>Hello</div>
+  <div class="bg-white shadow-sm rounded p-6 border border-gray-300">
+    <div class="border-b pb-6">
+      <AppPostForm />
+    </div>
+
+    <AppPost />
+    <AppPost />
+    <AppPost />
+    <AppPost />
+    <AppPost />
+  </div>
 </template>
 
 <script>
